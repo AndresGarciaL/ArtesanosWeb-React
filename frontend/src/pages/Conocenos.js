9bcad2e6be2c7c7c152a8658a27b644933e8501f
Marquez Peralta Gael Aldair <22393137@utcancun.edu.mx>
2023-07-03 01:33:49 -0500

    Se agrego Conocenos.html

--- frontend/src/pages/Conocenos.js
+++ frontend/src/pages/Conocenos.js
@@ -1,6 +1,8 @@
 //1.
 import React from 'react';
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/Conocenos.css";
 
 //2.
 function Conocenos() {
@@ -8,9 +10,61 @@ function Conocenos() {
     return (
         <>
             <Header />
-            <div className="container">
-                
+            <h2 className="tituloh2">¿Quiénes somos?</h2>
+            <div class="left">
+                <img id="img-conocenos" src={require("../images/img-contacto/conocenos.png")} alt="Imagen Conócenos" />
+                <div class="parrafos">
+                    <p class="parrafo1">Artesanos Web es un sitio web que apoya y promueve la artesanía. Es un espacio donde
+                        difundimos los productos
+                        e
+                        historias de artesanos, artistas y diseñadores.</p>
+                    <h3 class="preguntas">¿Por qué hecho a mano?</h3>
+                    <p class="parrafo1">Entendemos cómo, dónde y quién hace lo que compramos; es una parte esencial de ser un
+                        consumidor responsable.
+                        Además, comprar a un artesano impulsa la economía local y como consumidor, conoces específicamente a aquellos
+                        que se benefician de esta compra.</p>
+                </div>
             </div>
+
+            <p class="parrafo4">Queremos cambiar la cultura del consumo y al mismo tiempo apoyar la cultura corporativa con una
+                cadena de
+                valor
+                donde todos ganemos. Queremos celebrar la artesanía, el uso de materiales sostenibles y elogiar la creatividad
+                de quienes promueven una economía circular</p>
+
+            <div className="imgs2">
+                <div className="artesanos">
+                    <img className="fotos"
+                        src={require("../images/img-contacto/hecho-a-mano.png")} />
+                    <h3 className="preguntas">Solo hecho a mano</h3>
+                </div>
+                <div className="artesanos">
+                    <img className="fotos" src={require("../images/img-contacto/sostenible.png")} />
+                    <h3 className="preguntas">Cadena de valor sostenible</h3>
+                </div>
+                <div className="artesanos">
+                    <img className="fotos"
+                        src={require("../images/img-contacto/historias.png")} />
+                    <h3 className="preguntas">Historias</h3>
+                </div>
+            </div>
+
+            <h2 className="tituloh2">Nuestro trabajo</h2>
+            <img id="ODS"
+                src={require("../images/img-contacto/conocenos/ODS.png")} />
+            <p class="parrafo4">Desde Valéxico estamos aportando nuestro granito de arena para conseguir estos objetivos. Con
+                el trabajo que se
+                realiza desde nuestra zanja, ayudamos con los objetivos #1: Acabar con la pobreza, #8: Trabajo decente y
+                crecimiento económico y económico #12: Producción y consumo responsables.</p>
+            <div class="imgs2">
+                <img
+                    src={require("../images/img-contacto/conocenos/pobreza.png")} id="pobreza" />
+                <img
+                    src={require("../images/img-contacto/conocenos/trabajo.png")} id="trabajo" />
+                <img
+                    src={require("../images/img-contacto/conocenos/produccion.png")} id="produccion" />
+            </div>
+            <Footer />
         </>
     );
 }
